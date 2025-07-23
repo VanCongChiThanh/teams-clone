2cd10788d892207a9a994e74bad37738e04b1507
Văn Công Chí Thanh <vancongchithanh2004@gmail.com>
2025-07-23 16:22:45 +0700

    handle users

---
+++ server/src/controllers/users.controller.js
@@ -0,0 +1,31 @@
+import { getUsersService,getUsersBySearchService} from "../services/users.service";
+
+export const getUsers = async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+  try {
+    const { users, totalUsers, totalPages } = await getUsersService(
+      Number(page),
+      Number(limit)
+    );
+    res.status(200).json({
+      users,
+      totalUsers,
+      page: Number(page),
+      totalPages,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+export const getUsersBySearch = async (req, res) => {
+  const { searchQuery } = req.query;
+  try {
+    const users = await getUsersBySearchService(searchQuery);
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
---
+++ server/src/services/users.service.js
@@ -0,0 +1,20 @@
+import User from "../models/user";
+
+export const getUsersService = async (page,limit)=>{
+    const users = await User.find()
+      .select("name _id email profilePicture")
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .sort({ createdAt: -1 });
+    const totalUsers = await User.countDocuments();
+    return {
+        users,
+        totalUsers,
+        page,
+        totalPages: Math.ceil(totalUsers / limit)
+    };
+}
+export const getUsersBySearchService = async (searchQuery) => {
+    const users = await User.find({ _id: { $in: searchQuery.split(",") } });
+    return users;
+};
