863e0c57f9bba7dc93ec0fe2028347cbf45bcdf8
Văn Công Chí Thanh <vancongchithanh2004@gmail.com>
2025-07-24 21:27:11 +0700

    socket to join room

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,8 @@ import mongoose from 'mongoose';
 import authRoutes from './routes/auth.js';
 import conversationsRoutes from './routes/conversations.js';
 import usersRoutes from './routes/users.js';
-// import Server from "socket.io";
+import eventsRoutes from './routes/events.js';
+import Server from "socket.io";
 
 const app = express();
 dotenv.config();
@@ -20,13 +21,33 @@ app.get("/", (req, res) => {
 app.use("/auth", authRoutes);
 app.use("/conversations", conversationsRoutes);
 app.use("/users", usersRoutes);
+app.use("/events", eventsRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    const server = app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    const io = new Server(server, {cookie:false});
+    const users = {};
+    const socketToRoom = {};
+    io.on("connection", (socket) => {
+      console.log("User connected");
+      socket.on("join room",(roomId)=>{
+        if(users[roomId]){
+          users[roomId].push(socket.id);
+        }else{
+          users[roomId]=[socket.id];
+        }
+        socketToRoom[socket.id]=roomId;
+        socket.join(roomId)
+        const usersInThisRoom=users[roomId];
+        socket.emit("all user",usersInThisRoom);
+      });
+      
+    });
   })
   .catch((error) => console.error(error.message));
